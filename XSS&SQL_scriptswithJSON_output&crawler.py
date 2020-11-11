2abce09d0bd3008f165896c52aecf8133abe79e2
Big Moist <sanicthefastmaster@gmail.com>
2020-11-11 15:47:08 +0800

    Added except Exception for XSS/SQL script

--- XSS&SQL_scriptswithJSON_output&crawler.py
+++ XSS&SQL_scriptswithJSON_output&crawler.py
@@ -7,9 +7,11 @@ import json
 from pprint import pprint
 from bs4 import BeautifulSoup as bs
 from urllib.parse import urljoin
+
 s = requests.Session()
 br = mechanize.Browser()
 s.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36"
+
 def get_all_forms(url):
     soup = bs(requests.get(url).content, "html.parser")
     return soup.find_all("form")
@@ -149,7 +151,7 @@ def scan_sql_injection(url):
                     print("[+] Form:")
                     print(form_details)
                     break
-    except:
+    except Exception:
         pass
 def trying():
 	
